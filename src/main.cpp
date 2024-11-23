9988b7950593d04746ec9b2a1b521df748a93e57
sang-am <118132614+sang-am@users.noreply.github.com>
2024-11-23 14:49:27 +0530

    LibEase

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+#include "../include/User.hpp"
+#include "../include/Utils.hpp"
+
+int main()
+{
+      clearScreen();
+      printBanner();
+      pressEnterToContinue();
+      clearScreen();
+      menuBar();
+      prompt();
+      return 0;
+}
